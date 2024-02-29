498ae1759f4f6619b089a56ace115cd6ef41cb39
Ananism17 <anandiljar5@gmail.com>
2024-02-29 17:54:40 +0600

    CORS bug fixed

--- components/HomeComponents/ListComponent.jsx
+++ components/HomeComponents/ListComponent.jsx
@@ -2,23 +2,34 @@ import React, { useState, useEffect, useRef } from "react";
 
 //react-bootstrap
 import { Container, Row, Col, Table, Button } from "react-bootstrap";
+import { PaginationControl } from "react-bootstrap-pagination-control";
 
 //axios
 import axios from "axios";
 import { BASE_URL } from "../../base";
+
+//services
 import { toBanglaNumber } from "../Services/ToBangla";
+
+//icons
+import { MdEdit, MdDelete } from "react-icons/md";
 
 const ListComponent = ({ genre }) => {
   const [bookList, setBookList] = useState([]);
   const [loader, setLoader] = useState(true);
 
+  //pagination-variables
+  const [page, setPage] = useState(1);
+  const [total, setTotal] = useState(0);
+  const [perPage, setPerPage] = useState(0);
+
   useEffect(() => {
     fetchBookList();
-  }, []);
+  }, [page]);
 
   //book-list
   const fetchBookList = () => {
-    const apiUrl = BASE_URL + "api/books?page=1&genre=" + genre;
+    const apiUrl = BASE_URL + "api/books?page=" + page +"&genre=" + genre;
     const config = {
       withCredentials: true,
     };
@@ -29,6 +40,8 @@ const ListComponent = ({ genre }) => {
         console.log(response.data);
         if (response.data.status) {
           setBookList(response.data.books);
+          setTotal(response.data.totalBooks);
+          setPerPage(response.data.pageSize);
           setLoader(false);
         }
       })
@@ -37,12 +50,10 @@ const ListComponent = ({ genre }) => {
       });
   };
 
-  console.log(bookList);
-
   return (
     <>
       {loader ? (
-        <>Loading</>
+        <></>
       ) : (
         <>
           <Container>
@@ -55,6 +66,7 @@ const ListComponent = ({ genre }) => {
                       <th>লেখক</th>
                       <th>প্রকাশক</th>
                       <th>প্রকাশের সাল</th>
+                      <th>আপডেট/ ডিলেট</th>
                     </tr>
                   </thead>
                   <tbody>
@@ -64,12 +76,35 @@ const ListComponent = ({ genre }) => {
                         <td>{book.writer}</td>
                         <td>{book.publisher}</td>
                         <td>{toBanglaNumber(book.publishingYear)}</td>
+                        <td>
+                          <Button className="me-2" variant="secondary">
+                            <MdEdit />
+                          </Button>
+                          <Button variant="danger">
+                            <MdDelete />
+                          </Button>
+                        </td>
                       </tr>
                     ))}
                   </tbody>
                 </Table>
               </Col>
             </Row>
+            <Row className="mt-3">
+            <Col className="d-flex justify-content-center">
+              <PaginationControl
+                page={page}
+                // between={4}
+                total={total}
+                limit={perPage}
+                changePage={(page) => {
+                  setPage(page);
+                }}
+                ellipsis={1}
+                linkClassName="custom-pagination"
+              />
+            </Col>
+          </Row>
           </Container>
         </>
       )}
